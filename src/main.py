d429a7ee399642f02ff54040a4a9f82bf37d2e37
Aetheridon <lewisvr@protonmail.com>
2024-06-11 12:54:28 +0100

    basic landing page

--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def landing_page():
     return render_template("landing.html")
+
+@app.route("/signup")
+def signup_page():
+    return render_template("signup.html")
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
